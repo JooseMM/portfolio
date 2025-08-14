99f6a86c94cbbf76b2b95436384d40c81501c905
Jose <127054655+JooseMM@users.noreply.github.com>
2025-08-14 00:56:34 -0400

    Update kotlin.icon.tsx
    fix(netlify): remove unused import

--- src/shared/skills/components/kotlin.icon.tsx
+++ src/shared/skills/components/kotlin.icon.tsx
@@ -1,6 +1,5 @@
 import { useUI } from "../../utils/context/hook.context";
 import { ThemeOptions } from "../../utils/interfaces/Theme.enum";
-import { getSkillColor } from "../skillBar.utils";
 
 export const KotlinIcon = () => {
   const { currentTheme } = useUI();
